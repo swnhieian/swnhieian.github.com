733f0d57ed053788c1b6933f2c929a34a440be1a
Shi Weinan <swnhieian@126.com>
2018-11-09 04:14:11 +0800

    Updates

---
+++ precache-manifest.91bc0d128d4fd4aacf9a3d0326e8ad38.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c8bf4601ac4fb6e0a75f9f419919368",
+    "url": "/static/media/me.1c8bf460.jpg"
+  },
+  {
+    "revision": "0e8212c7dc0c93a50d796430c98225f1",
+    "url": "/static/media/bg.0e8212c7.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b60d95a6d0c47c2c2b02",
+    "url": "/static/js/main.b60d95a6.chunk.js"
+  },
+  {
+    "revision": "145ecc21c711e23d8b1b",
+    "url": "/static/js/1.145ecc21.chunk.js"
+  },
+  {
+    "revision": "b60d95a6d0c47c2c2b02",
+    "url": "/static/css/main.21dd6241.chunk.css"
+  },
+  {
+    "revision": "145ecc21c711e23d8b1b",
+    "url": "/static/css/1.d528fbc2.chunk.css"
+  },
+  {
+    "revision": "eddd9d870de1b5511a9a3ca352b498f3",
+    "url": "/index.html"
+  }
+];
